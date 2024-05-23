fa4210af92cf8432886af221d27d191c27f93731
Kgosiyagae Motabogi <ndosisetlole@gmail.com>
2024-05-23 21:32:01 +0200

    Added cards, Search and AddNote components

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import ProfileInfo from "../Cards/ProfileInfo";
+import { useNavigate } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
 
-export default function () {
+export default function Navbar() {
+  const [SearchQuery, setSearchQuery] = useState("");
+
+  const navigate = useNavigate;
+  const onLogout = () => {
+    navigate("/login");
+  };
+
+  const handleSearch = () => {};
+
+  const onClearSearch = () => {
+    setSearchQuery("");
+  };
   return (
     <div className="bg-white flex items-center justify-between px-6 py-6 drop-shadow">
       <h2 className="text-lg font-medium text-black py-2">Notes</h2>
+
+      <SearchBar
+        value={SearchQuery}
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
+      <ProfileInfo onLogout={onLogout} />
     </div>
   );
 }
---
+++ frontend/src/pages/Home/AddEditNote.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function AddEditNote() {
+  return (
+    <div className="">
+      <div className="flex flex-col gap-2">
+        <label className=" ">TITLE</label>
+        <input type="text" className="text-2xl text-slate-950 outline-none" placeholder="Go To Gym At 5" />
+      </div>
+      <div className="flex flex-col gap-2 mt-4">
+        <label className="input-label">CONTENT</label>
+        <textarea type="text" className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded" placeholder="Content" rows={10}></textarea>
+      </div>
+      <div className="mt-3">
+        <label className="input-label">TAGS</label>
+      </div>
+      <button className="btn-primary font-medium mt-5 p-3" onClick={() => {}}>
+        ADD
+      </button>
+    </div>
+  );
+}
+
+export default AddEditNote;
