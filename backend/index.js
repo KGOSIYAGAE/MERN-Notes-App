32daee1a36ff636c26acea4a1ef5a4d94f02fd69
Kgosiyagae Motabogi <ndosisetlole@gmail.com>
2024-05-28 00:06:31 +0200

    adde the .env file

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,17 @@
 import express from "express";
 import cors from "cors";
+import "dotenv/config";
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 //Middleware
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
 //Roustes
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.json({ data: "Hellow" });
+});
 
 //Server
 app.listen(PORT, () => {
